f08f48f99edd543c855993d586ed97c2e8612f95
Himani Babariya <himanibabariya@gmail.com>
2024-03-31 14:08:31 -0400

    Initial Setup

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div className="left">
+            
+
+        </div>
+        <div className="right">
+
+
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
