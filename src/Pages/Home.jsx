df3f43b3479eeea66d810b97fc8f7cdf6db99ad5
Himani Babariya <himanibabariya@gmail.com>
2024-07-04 22:04:05 -0400

    Style Updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ import Signup from '../Components/SignUp/Signup'
 const Home = () => {
   const [showSignUp, setSignUp] = useState(false)
 
-  const signUpForm = ()=>{
+  const signUpForm = () => {
     setSignUp(true)
   }
 
@@ -16,14 +16,14 @@ const Home = () => {
     <div>
       {showSignUp && <Signup />}
       <section id='home' className="home">
-        <div className="left"> 
-       
+        <div className="left">
+
           <h1>From Dispatch to Doorstep: Monitor Your Shipments Every Step of the Way!</h1>
           <form id='track' action="" method="get">
             <input type="text" placeholder='Enter tracking numbers...' />
             <button><img src={Arrow} alt="" /></button>
           </form>
-           <img src={DeliveryBoy} alt="" className='deliveryBoy'/>  
+          <img src={DeliveryBoy} alt="" className='deliveryBoy' />
         </div>
 
         <div className="right">
@@ -33,13 +33,32 @@ const Home = () => {
             <li>AliExpress</li>
             <li>Amazon</li>
             <li>Etsy</li>
-            <li></li>      
+            <li></li>
           </ul>
         </div>
       </section>
 
       <section id="services">
         <h1>Tracking Services</h1>
+
+        <h3>End-to-End Parcel tracking for over 200 countries</h3>
+        <div className="services-left">
+
+        <p>No corner of the globe is beyond our reach with ParcelTraker system.
+          Harnessing unparalleled speed, we scan through thousands of shops and logistics
+          providers simultaneously, delivering the latest updates on your parcel's journey,
+          no matter where it travels worldwide.</p>
+
+        <p>Enter your tracking number on our website and unlock real-time tracking
+          information for all your parcels and packages instantly. With ParcelTracking,
+          stay ahead of the curve and track with confidence, anytime, anywhere!</p>
+
+          <button>Explore Supported Countries</button>
+        </div>
+
+        <div className="services-right">
+          <img src="" alt="" />
+        </div>
       </section>
 
       <section id="contact">
